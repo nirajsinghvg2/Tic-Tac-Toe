56a7f0be31b64f467d636128b2bb8f6b21b13a0f
Niraj Singh <nirajsinghvg2@gmail.com>
2026-06-23 09:55:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let gameover = new Audio("gameover.mp3");
 let turn = "X";
 let isgameover = false;
 
-// Function to change the turn
+// Function to change the turn (0/X)
 const changeTurn = () => {
     return turn === "X" ? "0" : "X";
 }
